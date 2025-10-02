1e478a6d86bbc0caaef0ef5156cd2ef177bc911b
Gmoon <184715025+vanphunghoanggia@users.noreply.github.com>
2025-10-02 14:28:44 +0700

    Update Manager.java

--- src/main/java/beans/Manager.java
+++ src/main/java/beans/Manager.java
@@ -9,35 +9,35 @@ public class Manager extends User {
     /**
      * Add new books.
      */
-    public void addNewBook() {
+    public void addNewBook() { //không có tham số để thêm sách mới
 
     }
 
     /**
      * Modify existing books.
      */
-    public void modifyBook() {
+    public void modifyBook() { //không có tham số để sửa
 
     }
 
     /**
      * Place orders for books.
      */
-    public void placeOrder() {
+    public void placeOrder() { //không có tham số để đặt order
 
     }
 
     /**
      * Confirm orders.
      */
-    public void confirmOrder() {
+    public void confirmOrder() { // không có tham số order
 
     }
 
     /**
      * Promote registered customers to have managers credentials.
      */
-    public void promoteUser() {
+    public void promoteUser() { // không có tham số user để promote
 
     }
 
@@ -50,21 +50,21 @@ public class Manager extends User {
     /**
      * The total sales for books in the previous month.
      */
-    public void getTotalSales() {
+    public void getTotalSales() { // method thể hiện hành vi trả về dữ liệu nhưng lại sử dụng void
 
     }
 
     /**
      * The top 5 customers who purchase the most purchase amount in descending order for the last three months.
      */
-    public void getTopFiveCustomers() {
+    public void getTopFiveCustomers() { // method thể hiện hành vi trả về dữ liệu nhưng lại sử dụng void
 
     }
 
     /**
      * The top 10 selling books for the last three months.
      */
-    public void getTopTenBooks() {
+    public void getTopTenBooks() { // method thể hiện hành vi trả về dữ liệu nhưng lại sử dụng void
 
     }
 
