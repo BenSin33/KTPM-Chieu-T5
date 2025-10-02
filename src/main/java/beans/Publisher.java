32171eb12e86072b3f8edaebe455db0305c89143
Gmoon <184715025+vanphunghoanggia@users.noreply.github.com>
2025-10-02 15:24:59 +0700

    Update Publisher.java

--- src/main/java/beans/Publisher.java
+++ src/main/java/beans/Publisher.java
@@ -8,29 +8,29 @@ public class Publisher {
     private List<String> phones;
 
     // GETTERS
-    public String getName() {
+    public String getName() { // name có thể trả về null nếu chưa set
         return name;
     }
 
-    public List<String> getAddresses() {
+    public List<String> getAddresses() { // addresses có thể null nếu chưa set
         return addresses;
     }
 
-    public List<String> getPhones() {
+    public List<String> getPhones() { // phones có thể null nếu chưa set
         return phones;
     }
 
 
     // SETTERS
-    public void setName(String name) {
+    public void setName(String name) { // Tham số name có thể null hoặc rỗng, không kiểm tra trước khi gán
         this.name = name;
     }
 
-    public void setAddresses(List<String> addresses) {
+    public void setAddresses(List<String> addresses) { // Không kiểm tra addresses có bị null hay chứa phần tử null
         this.addresses = addresses;
     }
 
-    public void setPhones(List<String> phones) {
+    public void setPhones(List<String> phones) { // Không kiểm tra phones có bị null hay chứa phần tử null
         this.phones = phones;
     }
 }
